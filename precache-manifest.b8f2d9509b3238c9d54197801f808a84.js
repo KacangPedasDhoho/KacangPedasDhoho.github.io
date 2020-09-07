be5ef1f0099e40fffb71184ae7aea558b576c707
nata bagus arisandi <natabagus098@gmail.com>
2020-09-07 11:28:42 +0700

    menyalin isi folder Build

---
+++ precache-manifest.b8f2d9509b3238c9d54197801f808a84.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56525f1a37b4630c0af7757094346e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de2f96194f2ff5ce30b6",
+    "url": "/static/css/main.c9e9b0af.chunk.css"
+  },
+  {
+    "revision": "0d7c12246c76638464dc",
+    "url": "/static/js/2.79a6f2b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79a6f2b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2f96194f2ff5ce30b6",
+    "url": "/static/js/main.8b1404b5.chunk.js"
+  },
+  {
+    "revision": "fbb8d41c579664ca0a52",
+    "url": "/static/js/runtime-main.d33bf5e0.js"
+  },
+  {
+    "revision": "6721a304475d125d36ae516ec0caa0b7",
+    "url": "/static/media/IMG_2046.6721a304.JPG"
+  },
+  {
+    "revision": "6116bc253df15491eeade4a5aa1fc52c",
+    "url": "/static/media/gambar1.6116bc25.JPG"
+  },
+  {
+    "revision": "e4988921232b7cfa930a94710d90cbd6",
+    "url": "/static/media/gambar2.e4988921.JPG"
+  },
+  {
+    "revision": "e4988921232b7cfa930a94710d90cbd6",
+    "url": "/static/media/gambar3.e4988921.JPG"
+  },
+  {
+    "revision": "a85cf8dd45f50d6c4ddc245e69c31fb9",
+    "url": "/static/media/gambar4.a85cf8dd.JPG"
+  },
+  {
+    "revision": "f1e030ef07086afdf274ff7060c7c637",
+    "url": "/static/media/gambar5.f1e030ef.JPG"
+  },
+  {
+    "revision": "675123f1034769cd5e28492e312f2b09",
+    "url": "/static/media/gambar6.675123f1.JPG"
+  },
+  {
+    "revision": "d20919a0465bfa341e2d84f9c63a4abe",
+    "url": "/static/media/logo dhoho.d20919a0.png"
+  },
+  {
+    "revision": "8b47f677bd9ada389400ade66f25e7d0",
+    "url": "/static/media/tokopedia.8b47f677.png"
+  }
+]);
